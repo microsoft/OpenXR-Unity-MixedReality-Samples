4e54a8983f6ebbfcac3b682317b8854a2490c62d
Kurtis <kurtie@microsoft.com>
2021-04-02 17:29:14 -0700

    Improve hand scene by splitting different API examples (#6)
    * Split HandJointsManager into two
    
    * Update HandTracking.unity
    
    * Turn MRTK's hand vis off in this scene
    
    * Split Hand into private classes
    
    * Add hand clean-up code
    
    * Update comments and stale code

---
+++ BasicSample/Assets/HandTracking/Scripts/ToggleMRTKHandVisualization.cs
@@ -0,0 +1,53 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.OpenXR.Samples
+{
+    /// <summary>
+    /// Turns the MRTK hand visualization off when the scene is loaded, to defer to the sample scene's representation.
+    /// Returns the MRTK settings to their original state when the scene is unloaded.
+    /// </summary>
+    public class ToggleMRTKHandVisualization : MonoBehaviour
+    {
+        private bool originalHandMeshVisualization = true;
+        private bool originalHandJointVisualization = true;
+        private MixedRealityHandTrackingProfile handTrackingProfile;
+
+        /// <summary>
+        /// Turns the MRTK hand visualization off when the scene is loaded, to defer to the sample scene's representation.
+        /// </summary>
+        private void Start()
+        {
+            MixedRealityInputSystemProfile inputSystemProfile = CoreServices.InputSystem?.InputSystemProfile;
+            if (inputSystemProfile == null)
+            {
+                return;
+            }
+
+            handTrackingProfile = inputSystemProfile.HandTrackingProfile;
+            if (handTrackingProfile != null)
+            {
+                originalHandMeshVisualization = handTrackingProfile.EnableHandMeshVisualization;
+                originalHandJointVisualization = handTrackingProfile.EnableHandJointVisualization;
+                handTrackingProfile.EnableHandMeshVisualization = false;
+                handTrackingProfile.EnableHandJointVisualization = false;
+            }
+        }
+
+        /// <summary>
+        /// Returns the MRTK settings to their original state when the scene is unloaded.
+        /// </summary>
+        private void OnDestroy()
+        {
+            if (handTrackingProfile != null)
+            {
+                handTrackingProfile.EnableHandMeshVisualization = originalHandMeshVisualization;
+                handTrackingProfile.EnableHandJointVisualization = originalHandJointVisualization;
+            }
+        }
+    }
+}
