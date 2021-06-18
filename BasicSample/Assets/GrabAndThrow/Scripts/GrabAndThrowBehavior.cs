9ee99ab16477f6bd47c57c850117228d4befcd6b
Pooja Sama <sasama@microsoft.com>
2021-06-18 11:04:16 -0700

    addressed PR commnets, still some functionality pending

--- BasicSample/Assets/GrabAndThrow/Scripts/GrabAndThrowBehavior.cs
+++ BasicSample/Assets/GrabAndThrow/Scripts/GrabAndThrowBehavior.cs
@@ -6,7 +6,16 @@ using UnityEngine.XR.OpenXR.Features.Interactions;
 
 public class GrabAndThrowBehavior : MonoBehaviour
 {
-    private bool[] m_wasTriggerPressed = { false, false };
+    private bool[] m_wasRigidBodyPicked = { false, false };
+    private const float allowedDistanceFromRigidBody = 1f;
+    private float distanceFromRigidBody;
+    private bool canHoldRigidBody = true;
+    private Rigidbody rigidBody;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     private void Update()
@@ -16,7 +25,7 @@ public class GrabAndThrowBehavior : MonoBehaviour
         {
             InputDevice device = InputDevices.GetDeviceAtXRNode((i == 0) ? XRNode.RightHand : XRNode.LeftHand);
             bool deviceHasData = device.TryGetFeatureValue(CommonUsages.isTracked, out bool deviceIsTracked);
-            deviceHasData &= device.TryGetFeatureValue(CommonUsages.primaryButton, out bool isdeviceTapped);
+            deviceHasData &= device.TryGetFeatureValue(CommonUsages.primaryButton, out bool isDeviceTapped);
             deviceHasData &= device.TryGetFeatureValue(CommonUsages.deviceVelocity, out Vector3 deviceVelocity);
             deviceHasData &= device.TryGetFeatureValue(CommonUsages.deviceAngularVelocity, out Vector3 deviceAngularVelocity);
             deviceHasData &= device.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 devicePosition);
@@ -24,24 +33,34 @@ public class GrabAndThrowBehavior : MonoBehaviour
 
             if (deviceHasData && deviceIsTracked)
             {
-                if (m_wasTriggerPressed[i] && !isdeviceTapped)
+                distanceFromRigidBody = Vector3.Distance(transform.localPosition, devicePosition);
+                if(distanceFromRigidBody > allowedDistanceFromRigidBody)
                 {
-                    // Release the rigidbody using velocity
-                    // Note that Unity's velocity is not expected to match the input's velocity, this is yet to be addressed 
-                    GetComponent<Rigidbody>().velocity = deviceVelocity;
-                    GetComponent<Rigidbody>().angularVelocity = deviceAngularVelocity;
-                    GetComponent<Rigidbody>().isKinematic = false;
-                    m_wasTriggerPressed[i] = false;
+                    canHoldRigidBody = false;
+                }
+                if(canHoldRigidBody)
+                {
+                    if (m_wasRigidBodyPicked[i] && !isDeviceTapped)
+                    {
+                        // Release the rigidbody using velocity
+                        // Note that Unity's velocity is not expected to match the input's velocity, this is yet to be addressed 
+                        rigidBody.velocity = deviceVelocity;
+                        rigidBody.angularVelocity = deviceAngularVelocity;
+                        rigidBody.isKinematic = false;
+                        m_wasRigidBodyPicked[i] = false;
+                    }
+
+                    if (isDeviceTapped)
+                    {
+                        // Pick the rigidbody
+                        m_wasRigidBodyPicked[i] = true;
+                        transform.localPosition = devicePosition;
+                        transform.localRotation = deviceRotation;
+                        rigidBody.isKinematic = true;
+                    }
+
                 }
 
-                if (isdeviceTapped)
-                {
-                    // Pick the rigidbody
-                    m_wasTriggerPressed[i] = true;
-                    transform.localPosition = devicePosition + (deviceRotation * Vector3.forward);
-                    transform.localRotation = deviceRotation;
-                    GetComponent<Rigidbody>().isKinematic = true;
-                }
             }
         }
     }
