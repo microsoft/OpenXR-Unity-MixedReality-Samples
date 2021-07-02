829ba037662ef2f6b0ec947813a0e0534328aabb
Kurtis <kurtie@microsoft.com>
2021-07-02 10:23:58 -0700

    Rename and try to find a local ARMeshManager when none is provided

--- BasicSample/Assets/ARMesh/Scripts/ARMeshUpdateFollowsUser.cs
+++ BasicSample/Assets/ARMesh/Scripts/ARMeshUpdateFollowsUser.cs
@@ -9,7 +9,7 @@ namespace Microsoft.MixedReality.OpenXR.BasicSample
     /// <summary>
     /// Updates the mesh bounding volume every frame to follow the user's head.
     /// </summary>
-    public class MeshFollowUser : MonoBehaviour
+    public class ARMeshUpdateFollowsUser : MonoBehaviour
     {
         [SerializeField]
         private ARMeshManager meshManager = null;
@@ -17,9 +17,20 @@ namespace Microsoft.MixedReality.OpenXR.BasicSample
         [SerializeField]
         private Vector3 boundingExtents = Vector3.one * 3;
 
+        private void Awake()
+        {
+            if (meshManager == null && !TryGetComponent(out meshManager))
+            {
+                Debug.LogError($"No {nameof(ARMeshManager)} was provided to {nameof(ARMeshUpdateFollowsUser)} on {name}.");
+            }
+        }
+
         private void Update()
         {
-            meshManager.subsystem.SetBoundingVolume(Camera.main.transform.position, boundingExtents);
+            if (meshManager != null)
+            {
+                meshManager.subsystem.SetBoundingVolume(Camera.main.transform.position, boundingExtents);
+            }
         }
     }
 }
