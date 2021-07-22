599b57b6a34100f84f8396734247824c7fdc6949
Kurtis <kurtie@microsoft.com>
2021-07-22 08:49:44 -0700

    Add new flag for setting up app remoting for build (#38)

--- BasicSample/Assets/Build/BuildApp.cs
+++ BasicSample/Assets/Build/BuildApp.cs
@@ -1,11 +1,17 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using Microsoft.MixedReality.OpenXR.Remoting;
 using System;
 using System.Linq;
 using UnityEditor;
 using UnityEditor.Build.Reporting;
+using UnityEditor.XR.Management;
+using UnityEditor.XR.OpenXR.Features;
 using UnityEngine;
+using UnityEngine.XR.Management;
+using UnityEngine.XR.OpenXR;
+using UnityEngine.XR.OpenXR.Features;
 
 namespace Microsoft.MixedReality.OpenXR.BasicSample.Build
 {
@@ -59,6 +65,9 @@ namespace Microsoft.MixedReality.OpenXR.BasicSample.Build
                     case "-buildOutput":
                         buildPath = arguments[++i];
                         break;
+                    case "-appRemoting":
+                        SetUpAppRemoting(BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget));
+                        break;
                 }
             }
         }
@@ -67,6 +76,34 @@ namespace Microsoft.MixedReality.OpenXR.BasicSample.Build
         {
             return (from scene in sceneList.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
                     select scene.Trim()).ToArray();
+        }
+
+        private static void SetUpAppRemoting(BuildTargetGroup targetGroup)
+        {
+            Debug.Log($"Setting up app remoting for {targetGroup}");
+
+            const string AppRemotingPlugin = "Microsoft.MixedReality.OpenXR.Remoting.AppRemotingPlugin";
+            Type appRemotingFeature = typeof(AppRemoting).Assembly.GetType(AppRemotingPlugin);
+            if (appRemotingFeature == null)
+            {
+                Debug.LogError($"Could not find {AppRemotingPlugin}. Has this class been removed or renamed?");
+                return;
+            }
+
+            FeatureHelpers.RefreshFeatures(targetGroup);
+            OpenXRFeature feature = OpenXRSettings.ActiveBuildTargetInstance.GetFeature(appRemotingFeature);
+            if (feature == null)
+            {
+                Debug.LogError($"Could not load {AppRemotingPlugin} as an OpenXR feature. Has this class been removed or renamed?");
+                return;
+            }
+            feature.enabled = true;
+
+            XRGeneralSettings settings = XRGeneralSettingsPerBuildTarget.XRGeneralSettingsForBuildTarget(targetGroup);
+            if (settings != null)
+            {
+                settings.InitManagerOnStart = false;
+            }
         }
     }
 }
