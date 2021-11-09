809458570b53885c9179083c583fe076ab43ef07
Pooja Sama <sasama@microsoft.com>
2021-11-09 11:53:46 -0800

    removed unnecessary components and added disconnect to flat ui

--- RemotingSample/Assets/Scripts/AppRemoting.cs
+++ RemotingSample/Assets/Scripts/AppRemoting.cs
@@ -5,19 +5,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 
-namespace Microsoft.MixedReality.OpenXR.Samples
+namespace Microsoft.MixedReality.OpenXR.RemotingSample
 {
     /// <summary>
     /// Helper script for automatically connecting an OpenXR app to a specific remote device.
     /// </summary>
     public class AppRemoting : MonoBehaviour
     {
-        [SerializeField, Tooltip("The UI to be displayed in a 2D app window, when the remote session hasn't yet been established.")]
-        private GameObject flatUI = null;
-
-        [SerializeField, Tooltip("The UI to be displayed in the 3D app session, when remoting and XR have both been established.")]
-        private GameObject immersiveUI = null;
-
         [SerializeField, Tooltip("A text field to input the IP address of the remote device, such as a HoloLens.")]
         private UnityEngine.UI.InputField textInput = null;
 
@@ -31,6 +25,9 @@ namespace Microsoft.MixedReality.OpenXR.Samples
         private Remoting.RemotingListenConfiguration remotingListenConfiguration = new Remoting.RemotingListenConfiguration { ListenInterface = "0.0.0.0", HandshakeListenPort = 8265, TransportListenPort = 8266, MaxBitrateKbps = 20000 };
 
         private static readonly List<XRDisplaySubsystem> XRDisplaySubsystems = new List<XRDisplaySubsystem>();
+        private Remoting.ConnectionState m_connectionState = Remoting.ConnectionState.Disconnected;
+        private Remoting.DisconnectReason m_disconnectReason = Remoting.DisconnectReason.None;
+        private bool m_listenMode = false;
 
         private void Awake()
         {
@@ -53,15 +50,50 @@ namespace Microsoft.MixedReality.OpenXR.Samples
                     return;
                 }
             }
+        }
 
-            if (immersiveUI != null)
-            {
-                immersiveUI.SetActive(false);
-            }
+        private void Update()
+        {
+            var ip = textInput.text;
+            var connectPort = remotingConfiguration.RemotePort;
+            var listenPort = remotingListenConfiguration.TransportListenPort;
+            string connectMessage, listenMessage;
 
-            if (flatUI != null)
+            if (Remoting.AppRemoting.TryGetConnectionState(
+                out Remoting.ConnectionState connectionState,
+                out Remoting.DisconnectReason disconnectReason))
             {
-                flatUI.SetActive(true);
+                if (m_connectionState != connectionState || disconnectReason != m_disconnectReason)
+                {
+                    m_connectionState = connectionState;
+                    m_disconnectReason = disconnectReason;
+                    connectMessage = string.IsNullOrWhiteSpace(ip)
+                            ? $"No IP address was provided to {nameof(Remoting.AppRemoting)}."
+                                : m_connectionState == Remoting.ConnectionState.Connected
+                                    ? $"Connected to {ip}:{connectPort}."
+                                    : m_connectionState == Remoting.ConnectionState.Connecting
+                                        ? $"Connecting to {ip}:{connectPort}..."
+                                        : $"Disconnected to {ip}:{connectPort}. Reason is {m_disconnectReason}";
+                    listenMessage = m_connectionState == Remoting.ConnectionState.Connected
+                                    ? $"Connected on {listenPort}."
+                                    : m_connectionState == Remoting.ConnectionState.Connecting
+                                        ? $"Listening to incoming connection on {listenPort}..."
+                                        : $"Disconnected on {listenPort}. Reason is {m_disconnectReason}";
+                    if (!m_listenMode)
+                    {
+                        if (outputText.text != connectMessage)
+                        {
+                            outputText.text = connectMessage;
+                        }
+                    } else
+                    {
+                        if (outputText.text != listenMessage)
+                        {
+                            outputText.text = listenMessage;
+                        }
+
+                    }
+                }
             }
         }
 
@@ -83,51 +115,13 @@ namespace Microsoft.MixedReality.OpenXR.Samples
                 remotingConfiguration.RemoteHostName = textInput.text;
             }
 
-            if (string.IsNullOrWhiteSpace(remotingConfiguration.RemoteHostName))
-            {
-                if (outputText != null)
-                {
-                    outputText.text = $"No IP address was provided to {nameof(AppRemoting)}. Returning without connecting.";
-                }
-                return;
-            }
-
-            if (outputText != null)
-            {
-                outputText.text = $"Connecting to {remotingConfiguration.RemoteHostName}...";
-            }
-
             StartCoroutine(Remoting.AppRemoting.Connect(remotingConfiguration));
-
-            if (flatUI != null)
-            {
-                flatUI.SetActive(false);
-            }
-
-            if (immersiveUI != null)
-            {
-                immersiveUI.SetActive(true);
-            }
         }
 
         public void ListenToRemote()
         {
-            if (outputText != null)
-            {
-                outputText.text = $"Listening to the player connections";
-            }
-
+            m_listenMode = true; 
             StartCoroutine(Remoting.AppRemoting.Listen(remotingListenConfiguration));
-
-            if (flatUI != null)
-            {
-                flatUI.SetActive(false);
-            }
-
-            if (immersiveUI != null)
-            {
-                immersiveUI.SetActive(true);
-            }
         }
 
         /// <summary>
@@ -136,17 +130,6 @@ namespace Microsoft.MixedReality.OpenXR.Samples
         public void DisconnectFromRemote()
         {
             Remoting.AppRemoting.Disconnect();
-
-            if (immersiveUI != null)
-            {
-                immersiveUI.SetActive(false);
-            }
-
-            if (flatUI != null)
-            {
-                flatUI.SetActive(true);
-            }
-
             if (outputText != null)
             {
                 outputText.text = "Disconnected";
