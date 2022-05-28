bb85241b229c30a806392f5d7b6235c6603758af
yl-msft <yl@microsoft.com>
2022-05-27 18:11:47 -0700

    messed up condition.

--- SampleShared/Samples/TextPanel/RuntimeInfo.cs
+++ SampleShared/Samples/TextPanel/RuntimeInfo.cs
@@ -92,9 +92,9 @@ namespace Microsoft.MixedReality.OpenXR.Sample
 
         private string GetOriginMode()
         {
-            return (m_inputSubsystem == null && m_inputSubsystem.running)
-                ? "Unknown Origin Mode" 
-                : m_inputSubsystem.GetTrackingOriginMode().ToString();
+            return (m_inputSubsystem != null && m_inputSubsystem.running)
+                ? m_inputSubsystem.GetTrackingOriginMode().ToString()
+                : "Unknown Origin Mode";
         }
 
         private static XRInputSubsystem GetXRInputSubsystem()
