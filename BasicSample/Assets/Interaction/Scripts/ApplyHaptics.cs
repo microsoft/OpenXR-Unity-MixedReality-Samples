3c6763fdcdb47f72c604151e43dc7107d3f3bf2b
Devdeep Choudhury <dec@microsoft.com>
2022-06-10 15:45:16 -0700

    addressed more comments

---
+++ BasicSample/Assets/Interaction/Scripts/ApplyHaptics.cs
@@ -0,0 +1,73 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace Microsoft.MixedReality.OpenXR.BasicSample
+{
+    /// <summary>
+    /// Checks for haptics support in the controller and if supported, it sends an impulse when the trigger is pressed.
+    /// </summary>
+    public class ApplyHaptics : MonoBehaviour
+    {
+        private readonly List<InputDevice> controllerHapticDevices = new List<InputDevice>();
+
+        private void InitializeDevices()
+        {
+            controllerHapticDevices.Clear();
+            List<InputDevice> controllerInputDevices = new List<InputDevice>();
+            InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Controller, controllerInputDevices);
+            foreach (InputDevice controller in controllerInputDevices)
+            {
+                if (controller.TryGetHapticCapabilities(out HapticCapabilities hapticCapabilities) && hapticCapabilities.supportsImpulse)
+                {
+                    controllerHapticDevices.Add(controller);
+                }
+            }
+                Debug.Log($"controller count {controllerHapticDevices.Count}");
+        }
+
+        private void OnDeviceConnected(InputDevice device)
+        {
+            InitializeDevices();
+        }
+
+        private void OnDeviceDisconnected(InputDevice device)
+        {
+            InitializeDevices();
+        }
+
+        private void Start()
+        {
+            InitializeDevices();
+        }
+
+        private void OnEnable()
+        {
+            InputDevices.deviceConnected += OnDeviceConnected;
+            InputDevices.deviceDisconnected += OnDeviceDisconnected;
+        }
+
+        private void OnDisable()
+        {
+            InputDevices.deviceConnected -= OnDeviceConnected;
+            InputDevices.deviceDisconnected -= OnDeviceDisconnected;
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            foreach (InputDevice controller in controllerHapticDevices)
+            {   
+                if (controller.TryGetFeatureValue(CommonUsages.trigger, out float trigger) && trigger > 0)
+                {
+                    // send haptics impulse with channel set to 0 and amplitude set to the trigger value
+                    controller.SendHapticImpulse(0, trigger);
+                }
+            }
+        }
+    }
+}
