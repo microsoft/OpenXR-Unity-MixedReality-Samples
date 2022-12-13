cf3814ca2d066249ff53f3edcf47a4546af66393
Keaton Bonds <kebonds@microsoft.com>
2022-12-13 15:48:42 -0800

    reuse new remoting disconnect button scripts in RemotingSample

--- SampleShared/Samples/AppRemoting/Scripts/EnableOnlyDuringRemoting.cs
+++ SampleShared/Samples/AppRemoting/Scripts/EnableOnlyDuringRemoting.cs
@@ -7,7 +7,8 @@ using Microsoft.MixedReality.OpenXR.Remoting;
 namespace Microsoft.MixedReality.OpenXR.BasicSample
 {
     // Enables this GameObject during remoting scenarios and disables it otherwise.
-    // Uses AppRemoting events to observe changes in state even while the GameObject is inactive.
+    // This GameObject must be enabled by default in the scene, to ensure the Awake() method is called.
+    // If the app wants to display UI only while connected, such as a "Disconnect" button, this script can be used.
     public class EnableOnlyDuringRemoting : MonoBehaviour
     {
         void Awake()
@@ -18,6 +19,7 @@ namespace Microsoft.MixedReality.OpenXR.BasicSample
                 this.gameObject.SetActive(false);
             }
 
+            // Uses AppRemoting events to observe changes in state even while the GameObject is inactive.
             Remoting.AppRemoting.Connected += OnRemotingConnected;
             Remoting.AppRemoting.Disconnecting += OnRemotingDisconnecting;
         }
