c20274e6bd577ceba10fac0b71e5a7f63d691bf9
Devdeep Choudhury <dec@microsoft.com>
2023-07-24 14:40:06 -0700

    rebase

--- SampleScenarios/Scenarios/MarkerSample/Scripts/SampleMarker.cs
+++ SampleScenarios/Scenarios/MarkerSample/Scripts/SampleMarker.cs
@@ -1,6 +1,7 @@
 using Microsoft.MixedReality.OpenXR;
 using UnityEngine;
 using Microsoft.MixedReality.OpenXR.Sample;
+using System;
 
 namespace Microsoft.MixedReality.OpenXR.Sample
 {
@@ -8,14 +9,35 @@ namespace Microsoft.MixedReality.OpenXR.Sample
     {
         [SerializeField]
         private ARMarker m_marker;
+        [SerializeField]
+        private MeshRenderer m_markerRenderer;
+
         private string m_text = "Sample Marker";
+        private const int m_countToUpdateFrame = 10;
+        private int m_countUntilNextUpdate = 0;
         public string UpdateText()
         {
-            if (m_marker != null)
+            if (m_marker != null && m_countUntilNextUpdate-- <= 0)
             {
-                m_text = $"{m_marker.trackableId} {m_marker.trackingState}\nTimeSinceLastSeen (ms): {(Time.realtimeSinceStartup - m_marker.lastSeenTime) * 1000}";
+                m_countUntilNextUpdate = m_countToUpdateFrame;
+                m_text = $"{m_marker.trackableId} {m_marker.trackingState}\nDurationSinceLastSeen (s): {Math.Round(Time.realtimeSinceStartup - m_marker.lastSeenTime, 2)}\nLastSeenTime (s): {Math.Round(m_marker.lastSeenTime, 2)}";
             }
             return m_text;
+        }
+
+        public void Update()
+        {
+            if (m_marker != null && m_markerRenderer != null)
+            {
+                if (Time.realtimeSinceStartup - m_marker.lastSeenTime > 10)
+                {
+                    m_markerRenderer.material.color = Color.gray;
+                }
+                else
+                {
+                    m_markerRenderer.material.color = Color.red;
+                }
+            }
         }
     }
 }
