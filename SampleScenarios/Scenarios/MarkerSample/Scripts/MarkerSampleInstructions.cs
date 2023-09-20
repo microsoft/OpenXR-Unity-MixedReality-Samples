fd321d16848b528c7ae662508da8212f5d266639
Devdeep Choudhury <dec@microsoft.com>
2023-09-20 10:51:47 -0700

    addressed comment

--- SampleScenarios/Scenarios/MarkerSample/Scripts/MarkerSampleInstructions.cs
+++ SampleScenarios/Scenarios/MarkerSample/Scripts/MarkerSampleInstructions.cs
@@ -15,7 +15,7 @@ namespace Microsoft.MixedReality.OpenXR.Sample
 
 - Use the 'Change Transform Mode' button to alter the center of the marker between origin and geometric center.
 - Use the 'Change Default Transform Mode' button to change the default transform mode of newly detected markers.
-- A blue cube indicates a marker thats being tracked. A grey cube indicates a marker with limited tracking.";
+- A blue cube indicates a marker that's being tracked. A grey cube indicates a marker with limited tracking.";
         }
 
         string ITextProvider.UpdateText()
